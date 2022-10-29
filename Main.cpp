3acc2a8407dcaeac292fc42a2b092bca9e49458b
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-10-29 19:16:03 +0300

    Done

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,10 @@
-#define _USE_MATH_DEFINES
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+
 #include <iostream>
-#include <cmath>
+
 
 using namespace std;
 
@@ -9,58 +13,78 @@ using namespace std;
 //різницю в днях між цими датами.Для виконання цього
 //завдання необхідно також написати функцію, яка визначає, чи є рік високосним.
 
-
-int Difference(int dayx, int monthx, int yearx, int dayy, int monthy, int yeary)
+bool LeapYear(int year)
 {
-	return dayx - dayy, monthx - monthy, yearx - yeary;
+    bool res = false;
+    if (year % 4 == 0)
+        res = true;
+    if (year % 100 == 0)
+        res = false;
+    if (year % 400 == 0)
+        res = true;
+    return res;
+}
+int Date(int d, int m, int y)
+{
+    int i = d;
+    if (LeapYear(y) && (m > 2))
+        i += y * 366;
+    else i += y * 365;
+    switch (m - 1)
+    {
+    case 12: i += 31;
+    case 11: i += 30;
+    case 10: i += 31;
+    case  9: i += 30;
+    case  8: i += 31;
+    case  7: i += 31;
+    case  6: i += 30;
+    case  5: i += 31;
+    case  4: i += 30;
+    case  3: i += 31;
+    case  2: i += 28;
+    case  1: i += 31;
+    }
+    return i;
+}
+int difference(int day1, int day2, int mon1, int mon2, int year1, int year2)
+{
+    int i = Date(day2, mon2, year2) - Date(day1, mon1, year1) + 1;
+    return i;
 }
 
 int main()
 {
-	int dayx=0, int monthx=0, int yearx=0, int dayy=0, int monthy=0, int yeary=0;
+    bool res; 
+    int i, day1, day2;
+    int mon1; int mon2; int year1;  int year2; 
+    
+    bool LeapYear(int year); 
+    int Date(int d, int m, int y); 
+    int difference(int day1, int day2, int mon1, int mon2, int year1, int year2); 
 	
-	cout << "Enter date1 /day/month/year ->";
-	cin >> dayx, monthx, yearx;
+    cout << "Enter day1 ->";
+	cin >> day1;
 
-	cout << "Enter date1 /day/month/year ->";
-	cin >> dayy, monthy, yeary;
+    cout << "Enter month1 ->";
+    cin >> mon1;
 
-	for (int dayx = 0; dayx < 32; dayx++)
-	{
-		cout << dayx << "\t"; 
-	}
-	for (int monthx = 0; monthx < 13; monthx++)
-	{
-		cout << monthx << "\t";
-	}
-	for (int yearx = 0; yearx < 3000; yearx++)
-	{
-		if (yearx % 4 == 0)
-		{
-			cout << yearx << "\t";
-		}
-	}
+    cout << "Enter year1 ->";
+    cin >> year1;
+    cout << "This is a leap year? -> " << LeapYear << endl; 
 
-	for (int dayy = 0; dayy < 32; dayy++)
-	{
-		cout << dayy << "\t";
-	}
-	for (int monthy = 0; monthy < 13; monthy++)
-	{
-		cout << monthy << "\t";
-	}
-	for (int yeary = 0; yeary < 3000; yeary++)
-	{
-		if (yeary % 4 == 0)
-		{
-			cout << yeary << "\t";
-		}
-	}
-	
-       Difference(dayx, monthx, yearx, dayy, monthy, yeary);
+	cout << "Enter day2 ->";
+	cin >> day2;
 
-	cout << endl; 
+    cout << "Enter month2 ->";
+    cin >> mon2;
 
+    cout << "Enter year2 ->";
+    cin >> year2;
+    cout << "This is a leap year? -> " << LeapYear << endl;
+
+    cout << "The difference is -> " << difference << endl;
+ 
 	return 0;
 }
 
